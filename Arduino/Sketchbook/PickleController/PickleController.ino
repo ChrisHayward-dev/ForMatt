c00c1832320c58987c343addab81cb5e0235c0ab
ChrisHayward-dev <hayward@smu.edu>
2022-07-26 10:44:46 -0500

    initial

--- Arduino/Sketchbook/PickleController/PickleController.ino
+++ Arduino/Sketchbook/PickleController/PickleController.ino
@@ -62,8 +62,9 @@
 #define PUMPRELAY_MASK (1 << PUMPRELAY)
 #define FIRERELAY_MASK (1 << FIRERELAY)
 
-#define SWALL_MASK 0x003F
-#define SWALL_OFF  0x003F
+#define SWALL_MASK 0x00FF
+#define SWALL_OFF  0x00FF
+#define SWBUTTONS_MASK  (SWFILL_MASK|SWPURGE_MASK|SWFIRE_MASK|SWSEQ_MASK|SWAIRPURGE_MASK|SWAIRFILL_MASK)
 #define RELAYALL_MASK 0xFF00
 #define RELAYALL_OFF  0xFF00
 #define RELAYCONTPL_MASK (RELAYALL_MASK & ~ARMLED_MASK & ~AUTOLED_MASK)
@@ -131,7 +132,7 @@ bool readsw(uint16_t mask, uint16_t value) {
 
 void safeswitch(int sw, int led) {
   /* save switch checks that switch sw is in the OFF (safe) position
-   if not, it blinks the switch LED until it is put in the safe position
+    if not, it blinks the switch LED until it is put in the safe position
   */
   while (PCF.read(sw) == LOW) {
     PCF.write(led, HIGH);
@@ -205,7 +206,7 @@ void setup() {
     Serial.println("PCF is not connected!");
     errorStop(2);
   }
-  set16(RELAYALL_MASK,RELAYALL_OFF);
+  set16(RELAYALL_MASK, RELAYALL_OFF);
 
   Serial.println("PCF connected");
   { // start 1306 display
@@ -222,87 +223,170 @@ void setup() {
   }
   safeswitch(SWARM, ARMLED);
   safeswitch(SWAUTO, AUTOLED);
-  attachInterrupt(digitalPinToInterrupt(PCF_INTERRUPT),switchChange,FALLING);  // setup the switch detect interrupt
+  attachInterrupt(digitalPinToInterrupt(PCF_INTERRUPT), switchChange, FALLING); // setup the switch detect interrupt
   digitalWrite(GREENLED, HIGH);
   digitalWrite(REDLED, LOW);
 }
 
+enum pState { PROG, MANUAL, AUTO, STANDBY};
+pState primaryState = PROG;
+
 void loop() {
   uint16_t  switches = SWALL_OFF;
-  if(swChange) {
+  uint16_t  buttons;
+  display.clear();
+  if (swChange) {
+    delay(10);
     swChange = false;
-     switches = PCF.readButton16(SWALL_MASK);
-     Serial.print("Switch Change:");
-     Serial.println(switches,HEX);
+    switches = PCF.readButton16(SWALL_MASK);
+    Serial.print("Switch Change:");
+    Serial.println(switches, HEX);
+    switch (switches & (SWARM_MASK | SWAUTO_MASK)) {
+      case (SWARM_MASK | SWAUTO_MASK):
+        Serial.println("All off");
+        set16(RELAYALL_MASK, RELAYALL_MASK);
+        primaryState = PROG;
+        break;
+      case SWARM_MASK:
+        Serial.println("Auto on");
+        set16(RELAYALL_MASK, RELAYALL_MASK);
+        primaryState = STANDBY;
+        break;
+      case SWAUTO_MASK:
+        Serial.println("Armed");
+        set16(RELAYALL_MASK, RELAYALL_MASK & ~ARMRELAY_MASK & ~ARMLED_MASK);
+        primaryState = MANUAL;
+        break;
+      case 0:
+        Serial.println("Armed in Auto");
+        set16(RELAYALL_MASK, RELAYALL_MASK & ~(ARMRELAY_MASK | ARMLED_MASK | AUTOLED_MASK));
+        primaryState = AUTO;
+        break;
+    }
+    buttons = switches & SWBUTTONS_MASK;
+    Serial.print("Buttons:");
+    Serial.println(buttons,HEX);
+    switch (primaryState) {
+    case PROG:
+      break;
+    case STANDBY:
+      break;
+    case MANUAL:
+      switch (buttons) {
+        case SWBUTTONS_MASK:
+        Serial.println("No buttons pressed");
+        break;
+        case SWBUTTONS_MASK & ~SWFILL_MASK:
+        Serial.println("Filling");
+        display.println("FILL");
+        break;
+        case SWBUTTONS_MASK & ~SWPURGE_MASK:
+        Serial.println("Purging");
+        display.println("PURGE");
+        break;
+        case SWBUTTONS_MASK & ~SWFIRE_MASK:
+        Serial.println("Firing");
+        display.println("FIRE");
+        break;
+        case SWBUTTONS_MASK & ~SWSEQ_MASK:
+        Serial.println("Auto Seq");
+        display.println("SEQ");
+        break;
+        case SWBUTTONS_MASK & ~SWAIRFILL_MASK:
+        Serial.println("Air Fill");
+        display.println("Air Fill");
+        break;
+        case SWBUTTONS_MASK & ~SWAIRPURGE_MASK:
+        Serial.println("Air Purge");
+        display.println("Air Purge");
+        break;
+        case SWBUTTONS_MASK & ~(SWFILL_MASK | SWPURGE_MASK):
+        Serial.println("Fill & purge");
+        display.println("Fill+Pur");
+        break;
+        case SWBUTTONS_MASK & ~(SWAIRFILL_MASK | SWAIRPURGE_MASK):
+        Serial.println("Air fill & purge");
+        display.println("Air FP");
+        break;
+        default:
+        Serial.println("Unknown state");
+        display.println("Invalid");
+      }
+      break;
+    case AUTO:
+      break;
   }
-  // static uint state = 0;
-  // uint32_t startTime;
-  // uint16_t curSwitch;
-  // bool validState = false;
-  // digitalWrite(REDLED,HIGH);
-  // for (state = 0; state < NSTATES; state++) {
-  //   validState = false;
-  //   Serial.print("\tChecking: ");
-  //   Serial.print(m[state].swmask, HEX);
-  //   Serial.print(" for ");
-  //   Serial.println(m[state].swvalue, HEX);
-  //   if (readsw(m[state].swmask, m[state].swvalue)) {
-  //     Serial.print("State: ");
-  //     Serial.println(state);
-  //     Serial.print("Relay Mask: ");
-  //     Serial.println(m[state].relaymask, HEX);
-  //     Serial.print("Relay Set:  ");
-  //     Serial.println(m[state].relayvalue, HEX);
-  //     set16(m[state].relaymask, m[state].relayvalue);
-  //     validState = true;
-  //   }
-  // }
-  // digitalWrite(REDLED,LOW);
-  // /* if we are in auto and sequencing has been started
-  //    then initialize with a purge/fire cycle and then
-  //    commence fill purge fire cycles until we either have
-  //    a different switch state or have NSTACK fires
-  // */
-  // digitalWrite(GREENLED,HIGH);
-  // if (validState && (state == NSTATES)) {
-  //   Serial.println("Auto Mode");
-  //   set16(RELAYALL_MASK & ~ARMRELAY_MASK, PURGERELAY_MASK);
-  //   delay(250);
-  //   if (PCF.read(SWARM) == LOW) {
-  //     PCF.write(FIRERELAY, LOW);
-  //     delay(100);
-  //     PCF.write(FIRERELAY, HIGH);
-  //   }
+  display.display();
+}
 
-  //   for (int k = 0; k < NSTACK; k++) {
-  //     Serial.println("Fill");
-  //     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~(FILLRELAY_MASK | PUMPRELAY_MASK));               //start fill w/ purge closed
-  //     startTime = millis();
-  //     curSwitch = PCF.read16()&(SWALL_MASK & ~SWAUTO_MASK);
-  //     while(millis()-startTime < FILLTIME) {
-  //       if(curSwitch != (PCF.read16()&(SWALL_MASK & ~SWAUTO_MASK))) break;
-  //       yield();
-  //     }
-  //     if(curSwitch != (PCF.read16()&(SWALL_MASK & ~SWAUTO_MASK))) break;                                                                         //fill delay
-  //     Serial.println("Purge");
-  //     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~(FILLRELAY_MASK | PUMPRELAY_MASK | PURGERELAY_MASK)); //open purge
-  //     delay(PURGETIME);                                                                           //purge delay
-  //     Serial.println("Wait");
-  //     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~0);                                             //all closed
-  //     delay(DEADTIME);                                                                            //close delay
-  //     if (PCF.read(SWARM) == LOW) {
-  //       Serial.println("Fire");
-  //       set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~FIRERELAY_MASK);
-  //     }
-  //     Serial.println("Wait");
-  //     delay(FIRETIME);
-  //     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~0);
-  //     Serial.println("--Finished");
-  //     delay(DEADTIME);
-  //     if(PCF.read(SWAUTO) == HIGH) break;       //end sequence early if swauto is set off
-  //   }
-  //   Serial.println("Finished Stack");
-  // }
-  // digitalWrite(GREENLED,LOW);
-  yield();
+// static uint state = 0;
+// uint32_t startTime;
+// uint16_t curSwitch;
+// bool validState = false;
+// digitalWrite(REDLED,HIGH);
+// for (state = 0; state < NSTATES; state++) {
+//   validState = false;
+//   Serial.print("\tChecking: ");
+//   Serial.print(m[state].swmask, HEX);
+//   Serial.print(" for ");
+//   Serial.println(m[state].swvalue, HEX);
+//   if (readsw(m[state].swmask, m[state].swvalue)) {
+//     Serial.print("State: ");
+//     Serial.println(state);
+//     Serial.print("Relay Mask: ");
+//     Serial.println(m[state].relaymask, HEX);
+//     Serial.print("Relay Set:  ");
+//     Serial.println(m[state].relayvalue, HEX);
+//     set16(m[state].relaymask, m[state].relayvalue);
+//     validState = true;
+//   }
+// }
+// digitalWrite(REDLED,LOW);
+// /* if we are in auto and sequencing has been started
+//    then initialize with a purge/fire cycle and then
+//    commence fill purge fire cycles until we either have
+//    a different switch state or have NSTACK fires
+// */
+// digitalWrite(GREENLED,HIGH);
+// if (validState && (state == NSTATES)) {
+//   Serial.println("Auto Mode");
+//   set16(RELAYALL_MASK & ~ARMRELAY_MASK, PURGERELAY_MASK);
+//   delay(250);
+//   if (PCF.read(SWARM) == LOW) {
+//     PCF.write(FIRERELAY, LOW);
+//     delay(100);
+//     PCF.write(FIRERELAY, HIGH);
+//   }
+
+//   for (int k = 0; k < NSTACK; k++) {
+//     Serial.println("Fill");
+//     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~(FILLRELAY_MASK | PUMPRELAY_MASK));               //start fill w/ purge closed
+//     startTime = millis();
+//     curSwitch = PCF.read16()&(SWALL_MASK & ~SWAUTO_MASK);
+//     while(millis()-startTime < FILLTIME) {
+//       if(curSwitch != (PCF.read16()&(SWALL_MASK & ~SWAUTO_MASK))) break;
+//       yield();
+//     }
+//     if(curSwitch != (PCF.read16()&(SWALL_MASK & ~SWAUTO_MASK))) break;                                                                         //fill delay
+//     Serial.println("Purge");
+//     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~(FILLRELAY_MASK | PUMPRELAY_MASK | PURGERELAY_MASK)); //open purge
+//     delay(PURGETIME);                                                                           //purge delay
+//     Serial.println("Wait");
+//     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~0);                                             //all closed
+//     delay(DEADTIME);                                                                            //close delay
+//     if (PCF.read(SWARM) == LOW) {
+//       Serial.println("Fire");
+//       set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~FIRERELAY_MASK);
+//     }
+//     Serial.println("Wait");
+//     delay(FIRETIME);
+//     set16(RELAYALL_MASK & ~ARMRELAY_MASK, ~0);
+//     Serial.println("--Finished");
+//     delay(DEADTIME);
+//     if(PCF.read(SWAUTO) == HIGH) break;       //end sequence early if swauto is set off
+//   }
+//   Serial.println("Finished Stack");
+// }
+// digitalWrite(GREENLED,LOW);
+yield();
 }
